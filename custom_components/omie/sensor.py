0e81da62c4b484494c6ccd1d6115b1c6b9fd9924
Renato <renato.silva.an@gmail.com>
2023-09-25 23:49:19 +0100

    first commit

---
+++ custom_components/omie/sensor.py
@@ -0,0 +1,66 @@
+import json
+import logging
+import requests
+from datetime import datetime
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import ATTR_ATTRIBUTION
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "omie_spot"
+CONF_URL = "https://www.omie.es/sites/default/files/dados/NUEVA_SECCION/INT_PBC_EV_H_ACUM.TXT"
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    url = config[CONF_URL]
+
+    add_entities([OMIESpotSensor(url)], True)
+
+class OMIESpotSensor(SensorEntity):
+    def __init__(self, url, css_selector):
+        self._url = url
+        self._state = None
+        self._attributes = {}
+
+    @property
+    def name(self):
+        return "OMIE Spot Data"
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def extra_state_attributes(self):
+        return self._attributes
+
+    def update(self):
+        try:
+            response = requests.get(self._url)
+
+            if response.status_code == 200:
+                content = response.text
+                current_datetime = datetime.now()
+                formatted_datetime = current_datetime.strftime("%d/%m/%Y;%H;")
+
+                lines = content.split('\n')
+
+                found_line = None
+                for line in lines:
+                    if line.startswith(formatted_datetime):
+                        found_line = line
+                        break
+
+                if found_line:
+                    values = found_line.split(';')
+                    desired_value = values[3].strip()
+                    desired_value_float = float(desired_value.replace(',', '.'))
+
+                    self._state = "Data Parsed"
+                    self._attributes = desired_value_float / 1000
+                else:
+                    _LOGGER.error("Not found.")
+            else:
+                _LOGGER.error(f"Failed to retrieve data. Status code: {response.status_code}")
+
+        except Exception as e:
+            _LOGGER.error(f"An error occurred: {str(e)}")
